eee8412ff716caba9888167c50fe369928b8e8aa
Karoline Oliveira <karol.kamg92@gmail.com>
2019-11-21 15:39:41 -0300

    Tirando o teste de pesquisa

--- teste/features/step_definitions/pjdigital.rb
+++ teste/features/step_definitions/pjdigital.rb
@@ -1,15 +1,14 @@
 Dado("que acesso o sistema PJ Digital") do
-    visit 'http://pjdigital.lab.sistemas.sofisa.local/'
-  end
-  
-  Quando("informo o usuário {string} e senha {string}") do |usuário, senha|
-    find('#username').set usuário 
-    find('#password').set senha
-    find('#kc-login').click
-    sleep 3
-  end
-  
-  Então("login realizado com sucesso") do
-    name = find('span[class="so-header--user--info--name"]')
-    expect(name).to have_content 'Juliane Tabor Ignacio'
+  visit 'http://pjdigital.lab.sistemas.sofisa.local/'
+end
+
+Quando("informo o usuário {string} e senha {string}") do |usuário, senha|
+  find('#username').set usuário
+  find('#password').set senha
+  find('#kc-login').click
+end
+
+Então("login realizado com sucesso") do
+  nome = find('.so-header--user--info--name')
+  expect(nome).to have_content 'Juliane Tabor Ignacio'
 end
